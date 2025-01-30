db033516cb08f779c8d63cac2f28d69022c0d6f2
DaniWork <messadaniele@gmail.com>
2025-01-30 12:27:47 +0100

    genera 1 rung in serie (versione 1)

--- src/routine_creator.py
+++ src/routine_creator.py
@@ -1,17 +1,23 @@
 from gestione_xml import leggi_config_xml
-    
+import rung_generator as rg
 
 def main():
     print("Benvenuto nel generatore di routine rockwell!")
     
-    
-    
+    module_number = input("Quanti moduli hai?: ").strip()
+    module_names = []
+    for i in range(int(module_number)):
+        module_names.append(input(f"Nome del modulo {i+1}: ").strip())
+        #print(f"Modulo {i+1}")
+        
     # Esempio di utilizzo
     file_path = 'c:/Users/Administrator/Documents/Progetti/Python/RockwellRoutineCreator/src/config.xml'
     rungs = leggi_config_xml(file_path)
     # for rung in rungs:
     #     print(rung)
     
+    rung1 = rg.single_rung_creation(rungs[0], module_names)
+    print(rung1)
     
 if __name__ == "__main__":
     main()
---
+++ src/rung_generator.py
@@ -0,0 +1,27 @@
+
+
+def single_rung_creation(rung, module_names):
+    """
+    Crea una stringa concatenata con i campi di rung in questo ordine:
+    prefisso, per ogni module_names concatena il suo valore con ripetizione,
+    e infine concatena con suffisso. Tutti i campi sono separati da ";".
+
+    Args:
+        rung (dict): Dizionario contenente gli attributi del "rung".
+        module_names (list): Lista di nomi di moduli da concatenare con ripetizione.
+
+    Returns:
+        str: Stringa concatenata con i campi di rung.
+    """
+    # Inizia con il prefisso
+    result = rung['prefisso']
+    
+    # Concatena ogni module_name con ripetizione
+    for module_name in module_names:
+        str = rung['ripetizione'].replace("MODULO", module_name)
+        result += f"{str}"
+    
+    # Concatena il suffisso
+    result += f"{rung['suffisso']}"
+    
+    return result
